1f756ad322b59204af5a22917d68b2a647a40240
Luke Warrington <13trianglez@gmail.com>
2024-08-14 12:51:35 +0000

    transfer flow

---
+++ MatchIntensity.gd
@@ -0,0 +1,46 @@
+{
+    "MatchIntensity": [
+        {
+            "match_type": "Training Match",
+            "intensity_level": "Low",
+            "energy_expenditure": 0.5,
+            "injury_risk": 0.1,
+            "impact_on_form": "Minimal"
+        },
+        {
+            "match_type": "Friendly",
+            "intensity_level": "Medium",
+            "energy_expenditure": 0.7,
+            "injury_risk": 0.2,
+            "impact_on_form": "Moderate"
+        },
+        {
+            "match_type": "League Match",
+            "intensity_level": "High",
+            "energy_expenditure": 1.0,
+            "injury_risk": 0.3,
+            "impact_on_form": "Significant"
+        },
+        {
+            "match_type": "Cup Match",
+            "intensity_level": "High",
+            "energy_expenditure": 1.0,
+            "injury_risk": 0.3,
+            "impact_on_form": "Significant"
+        },
+        {
+            "match_type": "Championship Playoff",
+            "intensity_level": "Very High",
+            "energy_expenditure": 1.2,
+            "injury_risk": 0.4,
+            "impact_on_form": "Very Significant"
+        },
+        {
+            "match_type": "International Match",
+            "intensity_level": "Very High",
+            "energy_expenditure": 1.2,
+            "injury_risk": 0.4,
+            "impact_on_form": "Very Significant"
+        }
+    ]
+}
---
+++ scenes/UserInterface.gd
@@ -0,0 +1,43 @@
+UI 
+8 Button Grid
+
+# Preferences - Top Left
+
+# Continue - Top Right
+## Game Clock
+Number of increments in the day available determined by "Organisation" and Club Secretary Competence
+Starts with 2, AM and PM
+Advancing can be done with a button on the top right of the screen or by long swiping right. 
+User can set auto-skip on the calendar, pre-selecting ways to fill the time slot.
+
+---
+
+# Roster
+
+
+# Staff
+
+
+---
+
+# Club
+
+
+# Facilities
+
+
+---
+
+# Market 
+
+
+# Negotiations
+
+
+---
+
+# Contacts
+The phone is the source of information, data, communications and preferences.
+Number of messages or calls that a user can do per increment is determined by "Communication" attribute
+
+# Director 
