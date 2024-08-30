d915e2e09bca68d0d379c09754f2529290467991
Neutral Working <13trianglez@gmail.com>
2024-08-30 09:48:37 +0200

    Create TransferValueMultiplier.gd

---
+++ TransferValueMultiplier.gd
@@ -0,0 +1,15 @@
+TransferValueMultiplier = (AgeTransferMultiplier *
+                          PotentialTransferMultiplier *
+                          PositionTransferMultiplier *
+                          PlayerContractTransferMultiplier *
+                          PlayerHealthTransferMultiplier *
+                          FormTransferMultiplier *
+                          PlayerInternationalTransferMultiplier *
+                          PlayerPRTransferMultiplier *
+                          SellerFinancialStrength *
+                          BuyerFinancialStrength *
+                          PlayerDivisionTransferMultiplier *
+                          PlayerInterestTransferMultiplier *
+                          PlayerHypeTransferMultiplier)
+
+FinalTransferValue = BaseTransferValue * TransferValueMultiplier
