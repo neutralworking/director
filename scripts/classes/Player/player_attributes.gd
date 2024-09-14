a27feb9cbd2d12f086d78c6c4ba2b59e870883ea
Neutral Working <13trianglez@gmail.com>
2024-09-14 22:26:55 +0200

    Update player_attributes.gd

--- scripts/classes/Player/player_attributes.gd
+++ scripts/classes/Player/player_attributes.gd
@@ -1,40 +1,319 @@
 # player_attributes.gd
 
-enum Rating {
-    TERRIBLE = 1,
-    POOR = 2,
-    AVERAGE = 3,
-    GOOD = 4,
-    EXCELLENT = 5
+{
+  "attributes": [
+    {
+      "name": "Acceleration",
+      "type": "physical",
+      "benchmark": "Theo Walcott",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Aerial Duels",
+      "type": "physical",
+      "benchmark": "Peter Crouch",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Aggression",
+      "type": "tactical",
+      "benchmark": "Roy Keane",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Agility",
+      "type": "keeper",
+      "benchmark": "Manuel Neuer",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Anticipation",
+      "type": "mental",
+      "benchmark": "Vincent Kompany",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Awareness",
+      "type": "tactical",
+      "benchmark": "N'Golo Kante",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Balance",
+      "type": "physical",
+      "benchmark": "Eden Hazard",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Blocking",
+      "type": "tactical",
+      "benchmark": "Virgil van Dijk",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Carries",
+      "type": "technical",
+      "benchmark": "Luka Modric",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Clearances",
+      "type": "tactical",
+      "benchmark": "Raphael Varane",
+      "multiplier": 1.0
+    },
+    {
+      "name": "CloseRange",
+      "type": "technical",
+      "benchmark": "Roberto Firmino",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Communication",
+      "type": "mental",
+      "benchmark": "Jordan Henderson",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Composure",
+      "type": "mental",
+      "benchmark": "Thiago Alcantara",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Concentration",
+      "type": "mental",
+      "benchmark": "Sergio Busquets",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Creativity",
+      "type": "mental",
+      "benchmark": "Kevin De Bruyne",
+      "multiplier": 1.2
+    },
+    {
+      "name": "Crossing",
+      "type": "technical",
+      "benchmark": "Trent Alexander-Arnold",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Decisions",
+      "type": "mental",
+      "benchmark": "Jorginho",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Discipline",
+      "type": "tactical",
+      "benchmark": "Fabinho",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Duels",
+      "type": "physical",
+      "benchmark": "Casemiro",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Drive",
+      "type": "mental",
+      "benchmark": "James Milner",
+      "multiplier": 1.0
+    },
+    {
+      "name": "First Touch",
+      "type": "technical",
+      "benchmark": "Bernardo Silva",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Footwork",
+      "type": "keeper",
+      "benchmark": "Edouard Mendy",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Handling",
+      "type": "keeper",
+      "benchmark": "Edouard Mendy",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Heading",
+      "type": "physical",
+      "benchmark": "Virgil van Dijk",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Intensity",
+      "type": "tactical",
+      "benchmark": "Sadio Mane",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Interceptions",
+      "type": "tactical",
+      "benchmark": "Rodri",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Jumping",
+      "type": "physical",
+      "benchmark": "Virgil van Dijk",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Leadership",
+      "type": "mental",
+      "benchmark": "Jordan Henderson",
+      "multiplier": 1.1
+    },
+    {
+      "name": "LongRange",
+      "type": "technical",
+      "benchmark": "Toni Kroos",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Marking",
+      "type": "tactical",
+      "benchmark": "Ruben Dias",
+      "multiplier": 1.0
+    },
+    {
+      "name": "MidRange",
+      "type": "technical",
+      "benchmark": "Kevin De Bruyne",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Movement",
+      "type": "physical",
+      "benchmark": "Sadio Mane",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Pace",
+      "type": "physical",
+      "benchmark": "Kylian Mbappe",
+      "multiplier": 1.2
+    },
+    {
+      "name": "PassAccuracy",
+      "type": "technical",
+      "benchmark": "Toni Kroos",
+      "multiplier": 1.1
+    },
+    {
+      "name": "PassRange",
+      "type": "technical",
+      "benchmark": "Toni Kroos",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Penalties",
+      "type": "technical",
+      "benchmark": "Jorginho",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Positioning",
+      "type": "tactical",
+      "benchmark": "Virgil van Dijk",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Pressing",
+      "type": "tactical",
+      "benchmark": "Luis Suarez",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Reactions",
+      "type": "keeper",
+      "benchmark": "Edouard Mendy",
+      "multiplier": 1.2
+    },
+    {
+      "name": "Set Pieces",
+      "type": "mental",
+      "benchmark": "Trent Alexander-Arnold",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Shielding",
+      "type": "physical",
+      "benchmark": "Olivier Giroud",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Skills",
+      "type": "technical",
+      "benchmark": "Neymar",
+      "multiplier": 1.2
+    },
+    {
+      "name": "Stamina",
+      "type": "tactical",
+      "benchmark": "N'Golo Kante",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Tackling",
+      "type": "tactical",
+      "benchmark": "Virgil van Dijk",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Takeons",
+      "type": "technical",
+      "benchmark": "Neymar",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Tempo",
+      "type": "mental",
+      "benchmark": "Kevin De Bruyne",
+      "multiplier": 1.0
+    },
+    {
+      "name": "Throwing",
+      "type": "physical",
+      "benchmark": "Edouard Mendy",
+      "multiplier": 1.0
+    },
+    {
+      "name": "ThroughBalls",
+      "type": "technical",
+      "benchmark": "Kevin De Bruyne",
+      "multiplier": 1.2
+    },
+    {
+      "name": "Unpredictability",
+      "type": "mental",
+      "benchmark": "Lionel Messi",
+      "multiplier": 1.2
+    },
+    {
+      "name": "Volleys",
+      "type": "physical",
+      "benchmark": "Karim Benzema",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Vision",
+      "type": "mental",
+      "benchmark": "Kevin De Bruyne",
+      "multiplier": 1.1
+    },
+    {
+      "name": "Weak Foot",
+      "type": "physical",
+      "benchmark": "Lionel Messi",
+      "multiplier": 1.1
+    }
+  ]
 }
-
-const MENTAL_ATTRIBUTES = [
-    "Communication", "Concentration", "Drive", "Leadership", "Anticipation",
-    "Composure", "Decisions", "Tempo", "Creativity", "Set Pieces",
-    "Unpredictability", "Vision"
-]
-
-const PHYSICAL_ATTRIBUTES = [
-  "Duels", "Shielding", "Throwing", "Acceleration",
-    "Balance", "Movement", "Pace", "Aerial Duels", "Heading",
-    "Jumping", "Volleys", "Weak Foot"
-]
-
-const TACTICAL_ATTRIBUTES = [
-    "Aggression", "Awareness", "Discipline", "Interceptions", "Positioning", "Blocking",
-    "Clearances", "Marking", "Tackling", "Intensity", "Pressing",
-    "Stamina"
-]
-
-const TECHNICAL_ATTRIBUTES = [
-    "Carries", "First Touch", "Skills", "Takeons", "Crossing",
-    "PassAccuracy", "PassRange", "ThroughBalls", "CloseRange", "MidRange",
-    "LongRange", "Penalties"
-]
-
-const KEEPER_ATTRIBUTES = [
-    "Agility", "Footwork", "Handling", "Reactions"
-]
-
-const ALL_ATTRIBUTES = MENTAL_ATTRIBUTES + PHYSICAL_ATTRIBUTES + \
-                       TACTICAL_ATTRIBUTES + TECHNICAL_ATTRIBUTES + KEEPER_ATTRIBUTES
+```​​​​​​​
